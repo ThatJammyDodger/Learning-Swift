89589ac8e502ec065a9bf8f17b6aef9a00b51726
Jay Ambadkar <91419873+ThatJammyDodger@users.noreply.github.com>
2023-03-21 21:15:18 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jay Ambadkar  on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
