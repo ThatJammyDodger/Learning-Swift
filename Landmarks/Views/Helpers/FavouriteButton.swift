b1f7bb424368ab9aa5724e946ff15f9b50f493a6
Jay Ambadkar <91419873+ThatJammyDodger@users.noreply.github.com>
2023-03-25 18:20:34 +0000

    Finished 'Handling User Input'

---
+++ Landmarks/Views/Helpers/FavouriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavouriteButton.swift
+//  Landmarks
+//
+//  Created by Jay Ambadkar  on 25/03/2023.
+//
+
+import SwiftUI
+
+struct FavouriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favourite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavouriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouriteButton(isSet: .constant(true))
+    }
+}
